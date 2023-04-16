88588642c6c194fe47cc0d57c51e4a78f52d2f4c
Meir Armon <meir.a@claroty.com>
2023-04-16 13:15:33 +0300

    use db insteda of in memory

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, Session
+
+Base = declarative_base()
 
 
-todos = [
-    {
-        "id": "1",
-        "item": "Read a book."
-    },
-    {
-        "id": "2",
-        "item": "Cycle around town."
-    }
-]
+class TODO(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    item = Column(String)
+
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/todo")
+Base.metadata.create_all(bind=engine)
+session = Session(bind=engine)
 
 
 app = FastAPI()
@@ -38,12 +42,17 @@ async def read_root() -> dict:
 
 @app.get("/todo", tags=["todos"])
 async def get_todos() -> dict:
+    todos = session.query(TODO).order_by(TODO.id).all()
+
     return { "data": todos }
 
 
 @app.post("/todo", tags=["todos"])
 async def add_todo(todo: dict) -> dict:
-    todos.append(todo)
+    new_todo = TODO(item=todo["item"])
+    session.add(new_todo)
+    session.commit()
+
     return {
         "data": { "Todo added." }
     }
@@ -51,26 +60,25 @@ async def add_todo(todo: dict) -> dict:
 
 @app.put("/todo/{id}", tags=["todos"])
 async def update_todo(id: int, body: dict) -> dict:
-    for todo in todos:
-        if int(todo["id"]) == id:
-            todo["item"] = body["item"]
-            return {
-                "data": f"Todo with id {id} has been updated."
-            }
+    todo = session.query(TODO).get(id)
+
+    if not todo:
+        return {
+            "data": f"Todo with id {id} not found."
+        }
+
+    todo.item = body["item"]
+    session.commit()
 
     return {
-        "data": f"Todo with id {id} not found."
+        "data": f"Todo with id {id} has been updated."
     }
 
 
 @app.delete("/todo/{id}", tags=["todos"])
 async def delete_todo(id: int) -> dict:
-    for todo in todos:
-        if int(todo["id"]) == id:
-            todos.remove(todo)
-            return {
-                "data": f"Todo with id {id} has been removed."
-            }
+    session.query(TODO).filter(TODO.id == id).delete()
+    session.commit()
 
     return {
         "data": f"Todo with id {id} not found."
